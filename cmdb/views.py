b6939b4f74ee8bb4600ce8b38d222675ccaa4201
e <13118114093@163.com>
2021-03-17 14:30:28 +0800

    新增前端页面修改功能

--- cmdb/views.py
+++ cmdb/views.py
@@ -4,5 +4,18 @@ from . import models
 from  django.http import HttpResponse
 
 # Create your views here.
+import time
+
+def add_data():#新增接口数据
+    pass
+
+def update_data():#修改数据
+    pass
+def delete_data():#删除数据
+    pass
+def select_data():#查询数据
+    pass
 def page(request):
+    print(time.time())
+    print(request.POST)
     return  render(request,'home_page.html')
