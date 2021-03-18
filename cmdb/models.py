e36ba250e651c7036391f3198f8c7843d46c7657
e <13118114093@163.com>
2021-03-18 15:02:40 +0800

    继续改 保存一下

--- cmdb/models.py
+++ cmdb/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+import pymysql
+
+db = pymysql.connect(host='localhost', user='root', password='123456', port=3306, db='test')
+cursor = db.cursor()
+
+def add_data(project,api_name,api_url,api_data,api_header,api_method):
+    sql = '''insert INTO t_api_data (project,api_name,api_url,api_data,api_header,api_method,create_time,modify_time) VALUES('%s','%s','%s','%s','%s','%s',NOW(),NOW())'''%(project,api_name,api_url,api_data,api_header,api_method)
+    print(sql)
+    cursor.execute(sql)
+    db.commit()
+
+def select_data(api_name=None,project=None,create_user=None):
+
+    if api_name=='' and project=='' and create_user=='':
+        print('查询全表')
+
+        sql='''select * from t_api_data ;'''
+        print(sql)
+        cursor.execute(sql)
+        desc = cursor.description  # 获取字段的描述，默认获取数据库字段名称，重新定义时通过AS关键重新命名即可
+        data_dict = [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]  # 列表表达式把数据组装起来
+        print(data_dict)
+        return data_dict
+    else:
+        sql ='''select * from t_api_data where api_name='%s'  or project ='%s' or modify_user_code='%s'; '''%(api_name,project,create_user)
+        print(sql)
+        cursor.execute(sql)
+        desc = cursor.description  # 获取字段的描述，默认获取数据库字段名称，重新定义时通过AS关键重新命名即可
+        data_dict = [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]# 列表表达式把数据组装起来
+        print(data_dict)
+        return data_dict
--- cmdb/views.py
+++ cmdb/views.py
@@ -4,7 +4,7 @@ from . import models
 from  django.http import HttpResponse
 
 # Create your views here.
-import time
+from . import models
 
 def add_data():#新增接口数据
     pass
@@ -16,6 +16,36 @@ def delete_data():#删除数据
 def select_data():#查询数据
     pass
 def page(request):
-    print(time.time())
     print(request.POST)
-    return  render(request,'home_page.html')
+    print(request.POST.get('operation_name', None))
+    if request.method=='POST':
+        data=''
+
+        if request.POST.get('operation_name', None) == 'add':
+            project = request.POST.get('project', None)
+            project_name = request.POST.get('project_name', None)
+            url = request.POST.get('url', None)
+            data = request.POST.get('data', None)
+            header = request.POST.get('header', None)
+            method = request.POST.get('method', None)
+            models.add_data(project,project_name,url,data,header,method)
+        elif request.POST.get('operation_name', None) == 'select':
+            api_name = request.POST.get('api_name', None)
+            project = request.POST.get('project', None)
+            create_user = request.POST.get('create_user', None)
+            data = models.select_data(api_name,project,create_user)
+            data=data
+            print(type(data),data)
+            # msg = {'msg': data}
+            # print(msg,type(msg))
+            # print(msg['msg'][0])
+
+        else:
+            pass
+
+        return render(request, 'home_page.html', {'data': data})
+    else:
+        return render(request, 'home_page.html')
+
+
+
