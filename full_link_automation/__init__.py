18c0960ba1b79516d7f3df9a0743e9709b85b4da
Liuhong857 <13118114093@163.com>
2021-10-08 16:04:05 +0800

    调整为mysql配置，换环境需要改地址

---
+++ full_link_automation/__init__.py
@@ -0,0 +1,4 @@
+#告诉Django用pymysql代替默认的MySQLdb 连接MySQL数据库
+import pymysql
+pymysql.install_as_MySQLdb()
+
--- full_link_automation/settings.py
+++ full_link_automation/settings.py
@@ -76,13 +76,24 @@ WSGI_APPLICATION = 'full_link_automation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+#配置mysql
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'HOST':'127.0.0.1',
+        'PORT': 3306,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
